3207a4691eafdb0568f98e4d445f3b46842bc112
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-29 21:16:31 +0700

    controller to edit

--- src/main/java/com/codegym/controller/SmartphoneController.java
+++ src/main/java/com/codegym/controller/SmartphoneController.java
@@ -43,10 +43,20 @@ public class SmartphoneController {
         return modelAndView;
     }
 
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST,
+    @RequestMapping(value="/edit/{id}", method=RequestMethod.GET)
+    public ModelAndView editSmartphonePage(@PathVariable int id) {
+        ModelAndView mav = new ModelAndView("phones/edit-phone");
+        Smartphone smartphone = smartphoneService.findById(id);
+        mav.addObject("sPhone", smartphone);
+        return mav;
+    }
+
+    @RequestMapping(value="/edit/{id}", method=RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Smartphone deleteSmartphone(@PathVariable Integer id) {
-        return smartphoneService.remove(id);
+    public Smartphone editSmartphone(@PathVariable int id,
+                                     @RequestBody Smartphone smartphone) {
+        smartphone.setId(id);
+        return smartphoneService.save(smartphone);
     }
 }
