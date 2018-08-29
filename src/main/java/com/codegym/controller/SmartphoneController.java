513cc6e0c07f74fb1fa4f0533a6921b88af3679f
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-29 21:06:04 +0700

    create method to view smartphone list

--- src/main/java/com/codegym/controller/SmartphoneController.java
+++ src/main/java/com/codegym/controller/SmartphoneController.java
@@ -5,10 +5,7 @@ import com.codegym.service.SmartphoneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -29,5 +26,20 @@ public class SmartphoneController {
     @ResponseBody
     public Smartphone createSmartphone(@RequestBody Smartphone smartphone) {
         return smartphoneService.save(smartphone);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Iterable<Smartphone> allPhones() {
+        return smartphoneService.findAll();
+    }
+
+    @GetMapping("")
+    public ModelAndView allPhonesPage() {
+        ModelAndView modelAndView = new ModelAndView("phones/all-phones");
+
+        modelAndView.addObject("allphones", allPhones());
+        return modelAndView;
     }
 }
