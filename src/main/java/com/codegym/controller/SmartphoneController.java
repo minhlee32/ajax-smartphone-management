ef14f229cd78cf5026576773c5a0b9f8f268a8ab
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-29 20:35:41 +0700

    create SmartphoneController

---
+++ src/main/java/com/codegym/controller/SmartphoneController.java
@@ -0,0 +1,33 @@
+package com.codegym.controller;
+
+import com.codegym.model.Smartphone;
+import com.codegym.service.SmartphoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/smartphones")
+public class SmartphoneController {
+
+    @Autowired
+    private SmartphoneService smartphoneService;
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public ModelAndView updateForm() {
+        ModelAndView mav = new ModelAndView("phones/new-phone");
+        mav.addObject("sPhone", new Smartphone());
+        return mav;
+    }
+
+    @RequestMapping(value = "/createNew", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Smartphone createSmartphone(@RequestBody Smartphone smartphone) {
+        return smartphoneService.save(smartphone);
+    }
+}
